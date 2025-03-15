ed07f52bd4cea9fe2facc34fe93e5db2c62c7b79
LuanRodrigues <luanr@ymail.com>
2025-03-15 19:22:22 -0300

    extract funcionando

---
+++ app/pipeline/extract.py
@@ -0,0 +1,39 @@
+import pandas as pd
+from pathlib import Path
+import os
+import glob
+from typing import List
+
+'''
+Função para ler arquivos de uma pasta
+e retornar uma lista de dataframes
+
+args: input_path (str): caminho da pasta com as arquivos
+
+return: lista de dataframes
+glob é para listar todos os arquivosde um pasta
+'''
+
+path = "data/input"
+
+def extract_from_excel(path: str) -> List[pd.DataFrame]:
+    all_files = glob.glob(os.path.join(path,"*.xlsx"))
+
+    data_frame_list = []
+    for file in all_files:
+        data_frame_list.append(pd.read_excel(file))
+        df = pd.concat(data_frame_list)
+
+    return df
+
+
+
+
+if __name__ == "__main__":
+    data_frame_list = extract_from_excel(path)
+    print(data_frame_list)
+
+    # Linha de teste
+    # sabendo q se mesmo ao colocar esse if 
+    # ao chamar qualquer função desse módulo o hello word será desconsiderado
+    print('hello word')
