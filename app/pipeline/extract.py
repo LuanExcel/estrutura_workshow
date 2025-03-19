d60deb6400939f7c7336f70076705a9bebb5fa53
LuanRodrigues <luanr@ymail.com>
2025-03-19 04:49:07 -0300

    update

--- app/pipeline/extract.py
+++ app/pipeline/extract.py
@@ -4,28 +4,29 @@ import os
 import glob
 from typing import List
 
-'''
-Função para ler arquivos de uma pasta
-e retornar uma lista de dataframes
 
-args: input_path (str): caminho da pasta com as arquivos
-
-return: lista de dataframes
-glob é para listar todos os arquivosde um pasta
-'''
 
 path = "data/input"
 
 def extract_from_excel(path: str) -> List[pd.DataFrame]:
+
+    '''
+    Função para ler arquivos de uma pasta
+    e retornar uma lista de dataframes
+
+    args: input_path (str): caminho da pasta com as arquivos
+
+    return: lista de dataframes
+    glob é para listar todos os arquivosde um pasta
+    '''
+
+
     all_files = glob.glob(os.path.join(path,"*.xlsx"))
 
     data_frame_list = []
     for file in all_files:
         data_frame_list.append(pd.read_excel(file))
     return data_frame_list
-
-
-
 
 if __name__ == "__main__":
     data_frame_list = extract_from_excel(path)
--- app/pipeline/transform.py
+++ app/pipeline/transform.py
@@ -2,9 +2,12 @@ import pandas as pd
 from typing import List
 
 
-'''
-Função para trasformas uma lista de dataframe em um unico df
-'''
+
 
 def concat_data_frames(data_frame_list: List[pd.DataFrame]) -> pd.DataFrame:
+
+    '''
+    Função para trasformas uma lista de dataframe em um unico df
+    '''
+
     return pd.concat(data_frame_list, ignore_index=True)
